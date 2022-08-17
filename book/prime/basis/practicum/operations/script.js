7abbae991bf066afb7564f79832265f4fd8a7edb
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-17 19:42:38 +0300

    javascript/book/prime/basis/practicum/operations/ done

---
+++ book/prime/basis/practicum/operations/script.js
@@ -0,0 +1,39 @@
+// №1
+// let secInDay = 24 * 60 * 60;
+// console.log(secInDay);
+
+// №2
+// let secIn30Days = 30 * 24 * 60 * 60;
+// console.log(secIn30Days);
+
+// №3
+// let secInYear = 365 * 24 * 60 * 60;
+// console.log(secInYear);
+
+// №4
+// let minInDay = 24 * 60;
+// console.log(minInDay);
+
+// // №5
+// let minInYear = 365 * 24 * 60;
+// console.log(minInYear);
+
+// // №6
+// let byteInMgByte = 1024 * 1024;
+// console.log(byteInMgByte);
+
+// // №7
+// let byteInGgByte = 1024 * 1024 * 1024;
+// console.log(byteInGgByte);
+
+// // №8
+// let byteIn10GgByte = 10 * 1024 * 1024 * 1024;
+// console.log(byteIn10GgByte);
+
+// // №9
+// let byteInTByte = 1024 * 1024 * 1024 * 1024;
+// console.log(byteInTByte);
+
+// // №10
+// let KbyteInTByte = 1024 * 1024 *1024;
+// console.log(KbyteInTByte);
