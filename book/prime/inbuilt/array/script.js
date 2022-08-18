7d4c969571ab8e938fe19d876fe03389c2db8918
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-18 19:49:57 +0300

    done

---
+++ book/prime/inbuilt/array/script.js
@@ -0,0 +1,50 @@
+// №1
+// let arr = [1, 2, 3];
+// arr.push(4, 5, 6);
+// console.log(arr);
+
+// №2
+// let arr = [1, 2, 3];
+// arr.unshift(4, 5, 6);
+// console.log(arr);
+
+// №3
+// let arr = [1, 2, 3];
+// console.log(arr.shift());
+
+// №4
+// let arr = [1, 2, 3];
+// console.log(arr.pop());
+
+// №5
+// let arr = [1, 2, 3, 4, 5];
+// arr.slice(4, 6);
+// console.log(arr.slice(0, 3));
+
+// №6
+// let arr = [1, 2, 3, 4, 5];
+// let arr2 = arr.slice(3, 5);
+// console.log(arr2)
+
+// №7
+// let arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 2)
+// console.log(arr);
+
+// №8
+// let arr = [1, 2, 3, 4, 5];
+// arr.splice(3, 0, 'a', 'b', 'c')
+// console.log(arr);
+
+// №9
+// let arr = [1, 2, 3, 4, 5];
+// arr.splice(1, 0, 'a', 'b');
+// arr.splice(6, 0, 'c');
+// arr.splice(8, 0, 'e');
+// console.log(arr);
+
+// №10
+// console.log([1, 2, 3, 4, 5].indexOf(3));
+
+// №11
+// console.log([1, 2, 3, 4, 5].includes(3));
