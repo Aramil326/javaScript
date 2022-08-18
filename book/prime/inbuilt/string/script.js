09766be8184b3fbbfdc90e2d136be850522cc8cc
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-18 19:37:57 +0300

    done

---
+++ book/prime/inbuilt/string/script.js
@@ -0,0 +1,67 @@
+// №1
+// console.log('js'.toUpperCase());
+
+// №2
+// console.log('JS'.toLowerCase());
+
+// №3
+// console.log('я учу javascript!'.substr(2, 3));
+// console.log('я учу javascript!'.substr(6, 10));
+// console.log('я учу javascript!'.substring(2, 5));
+// console.log('я учу javascript!'.substring(6, 16));
+// console.log('я учу javascript!'.slice(2, 5));
+// console.log('я учу javascript!'.slice(6, 16));
+
+// №4
+// console.log('abcde'.indexOf('c'));
+
+// №5
+// if ('abcde'.indexOf('a') !== -1) {
+//   console.log('abcde'.indexOf('a'));
+// }
+
+// №6
+// if ('abcde'.indexOf('a') == 0) {
+//   console.log('abcde'.indexOf('a'));
+// }
+
+// №7
+// if ('abcdea'.lastIndexOf('a')) {
+//   console.log('abcde'.indexOf('a'));
+// }
+
+// №8
+// if ('http://abcdea'.indexOf('http://') == 0) {
+//   console.log('http://abcdea'.indexOf('a'));
+// }
+
+// №9
+// if ('http://abcdea.html'.lastIndexOf('.html')) {
+//   console.log('http://abcdea'.indexOf('a'));
+// }
+
+// №10
+// if ('http://abcdea.html'.startsWith('http://')) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// №11
+// if ('http://abcdea.html'.endsWith('.html')) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// №12
+// let arr = '1-2-3-4-5'.split('-');
+// console.log(arr);
+
+// №13
+// let arr = '12345'.split('');
+// console.log(arr);
+
+// №14
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.join('-'));
