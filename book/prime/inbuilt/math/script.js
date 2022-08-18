1fa3ca19019b55a3a94c7309dc45aeb67aef8b2c
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-18 18:59:29 +0300

    done

---
+++ book/prime/inbuilt/math/script.js
@@ -0,0 +1,51 @@
+// №1
+// console.log(Math.pow(2, 10));
+
+// №2
+// console.log(Math.sqrt(245));
+
+// №3
+// let arr = [4, 2, 5, 19, 13, 0, 10],
+//   sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum += Math.pow(arr[i], 3)
+// }
+// console.log(Math.sqrt(sum));
+
+
+// №4
+// console.log(Math.round(Math.sqrt(379)));
+// console.log(Math.sqrt(379).toFixed(1));
+// console.log(Math.sqrt(379).toFixed(2));
+
+// №5
+// let obj = {};
+// obj['floor'] = Math.floor(Math.sqrt(587));
+// obj['ceil'] = Math.ceil(Math.sqrt(587));
+// console.log(obj);
+
+// №6
+// console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
+// console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+
+// №7
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// console.log(getRandomInt(1, 100));
+
+// №8
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// let arr = [];
+// for (let i = 0; i < 10; i++) {
+//   arr.push(getRandomInt(0, 10));
+// }
+// console.log(arr);
+
+// №9
+// let a = 5, b = 7;
+// console.log(Math.abs(a - b));
