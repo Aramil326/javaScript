38ab3bd45d509ba8e46cec0f9405e071607641ab
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-19 13:30:43 +0300

    done

---
+++ book/prime/functions/function-declaration-expression/script.js
@@ -0,0 +1,32 @@
+//1
+// function func() {
+//   console.log('!');
+// } // Function Declaration
+
+//2
+// let func = function() {
+// 	console.log('!');
+// } //Function Expression
+
+//3
+// func();
+// function func() {
+//   console.log('!');
+// } // Function Declaration
+//Доступна
+
+//4
+// func();
+// let func = function () {
+//   console.log('!');
+// } //Function Expression
+//Недоступна
+
+//5
+// let func1 = function () { console.log('!') };
+// let func2 = function () {
+//   console.log('!')
+// };
+// function func3() {
+//   console.log('!')
+// };
