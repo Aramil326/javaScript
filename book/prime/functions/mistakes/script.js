f17c9d4263ebbfb9e228e3a4e5d8b9fdeb8cebbe
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-19 15:14:49 +0300

    done

---
+++ book/prime/functions/mistakes/script.js
@@ -0,0 +1,128 @@
+// 1
+// function func1() {
+//   return 3;
+// }
+// function func2() {
+//   return 5;
+// }
+
+// console.log(func1() + func2());
+
+// 2
+// function sum(arr) {
+//   let res = 0;
+
+//   for (let elem of arr) {
+//     res += elem;
+//   }
+//   return res;
+// }
+
+// console.log(sum([1, 2, 3, 4, 5]));
+
+// 3
+// let arr = [1, 2, 3, 4, 5];
+
+// function func(arr) {
+//   let res = 0;
+
+//   for (let elem of arr) {
+//     res += elem;
+//   }
+
+//   console.log(res);
+// }
+
+// func(arr);
+
+// 4
+// function func1() {
+//   return 3;
+// }
+// function func2() {
+//   return 5;
+// }
+
+// console.log(func1() + func2());
+
+// 5
+// function sum(arr) {
+//   let sum = 0;
+
+//   for (let elem of arr) {
+//     sum += elem;
+//   }
+
+//   return sum;
+// }
+
+// console.log(sum([1, 2, 3, 4, 5]));
+
+// 6
+// let res = sum([1, 2, 3, 4, 5]);
+// console.log(res);
+
+// function sum(arr) {
+//   let sum = 0;
+
+//   for (let elem of arr) {
+//     sum += elem;
+//   }
+
+//   return sum;
+// }
+
+// 7
+// function add(num) {
+//   if (num <= 9) {
+//     return '0' + num;
+//   } else {
+//     return num
+//   }
+// }
+
+// console.log(add(10));
+
+// 8
+// let arr = [1, 2, 3, 4, 5];
+// let sum = sumOfArr(arr);
+// console.log(sum);
+
+// function sumOfArr(arr) {
+//   let res = 0;
+
+//   for (let elem of arr) {
+//     res += elem;
+//   }
+
+//   return res;
+// }
+
+// 9
+// let num = 12345;
+// let res = getDigitsSum(num);
+// console.log(res);
+
+// function getDigitsSum(num) {
+//   let arr = String(num).split('');
+//   let sum = 0;
+
+//   for (let elem of arr) {
+//     sum += +elem;
+//   }
+
+//   return sum;
+// }
+
+// 10
+// console.log(isPrime(13));
+
+// function isPrime(num) {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i !== 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
