a0e5e0f1ef83c873cca70c561aca4f52374927d8
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-19 13:11:10 +0300

    done

---
+++ book/prime/functions/global-variables-changing/script.js
@@ -0,0 +1,79 @@
+// №1
+// let num = 1;
+
+// function func() {
+//   num = 2;
+// }
+// func();
+
+// console.log(num); //2
+
+// №2
+// let num = 1;
+
+// function func() {
+// 	num = 2;
+// }
+// func();
+
+// console.log(num); //1
+
+// №3
+// let num = 1;
+
+// function func() {
+// 	let num = 2;
+// }
+// func();
+
+// console.log(num); //1
+
+// №4
+// let num = 1;
+
+// function func() {
+// 	let num = 2;
+// }
+
+// console.log(num);
+// func(); //1
+
+// №5
+// let num = 1;
+
+// function func() {
+// 	num = 2;
+// }
+
+// console.log(num); //1
+// func();
+
+// №6
+// let num = 1;
+
+// function func() {
+// 	num++;
+// }
+
+// func();
+// func();
+// func();
+// console.log(num); //4
+
+// №7
+// function func() {
+// 	num = 2;
+// }
+
+// let num = 1;
+// console.log(num); //1
+// func();
+
+// №8
+// function func() {
+// 	num = 2;
+// }
+
+// let num = 1;
+// func();
+// console.log(num); //2
