d91666986764103a4bafc5f319ad18a1770f29da
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-19 13:39:50 +0300

    done

---
+++ book/prime/functions/functional-expressions-nuances/script.js
@@ -0,0 +1,126 @@
+//1
+// function func() {
+// 	console.log('!');
+// } //Function Declaration
+
+//2
+// let func = function() {
+// 	console.log('!');
+// } //Function Expression
+
+//3
+// +function() {
+// 	console.log('!');
+// } //Function Expression
+
+//4
+// !function func() {
+// 	console.log('!');
+// } //Function Expression
+
+//5
+// -function func() {
+// 	console.log('!');
+// } //Function Expression
+
+//6
+// 1 + function func() {
+// 	console.log('!');
+// } // Function Expression
+
+//7
+// (function func() {
+// 	console.log('!');
+// }) //Function Expression
+
+//8
+// console.log(
+// 	function() {
+// 		console.log('!');
+// 	}
+// ); //Function Expression
+
+//9
+// let test = function func() {
+// 	console.log('!');
+// } //Function Expression
+
+//10
+// console.log(
+// 	function func() {
+// 		console.log('!');
+// 	}
+// ); //Function Expression
+
+//11
+// +function func() {
+// 	console.log('!');
+// } //Function Expression
+
+//12
+// function func() {
+// 	console.log('!');
+// } //Function Declaration
+
+//13
+// -
+// function func() {
+// 	console.log('!');
+// } //Function Expression
+
+//14
+// -1;
+// function func() {
+// 	console.log('!');
+// } //Function Declaration
+
+//15
+// -1
+// function func() {
+// 	console.log('!');
+// } //Function Declaration
+
+//16
+// 1
+// function func() {
+// 	console.log('!');
+// } //Function Declaration
+
+//17
+// -1-
+// function func() {
+// 	console.log('!');
+// } // Function Expression
+
+//18
+// function func() {
+// 	console.log('!');
+// } //Function Declaration
+// +1;
+
+//19
+// function func() {
+// 	console.log('!');
+// } + 1; //Function Declaration
+
+//20
+// +function func() {
+// 	console.log('!');
+// } + 1; //Function Expression
+
+//21
+// +
+// function func() {
+// 	console.log('!');
+// } + 1; //Function Expression
+
+//22
+// + 1
+// function func() {
+// 	console.log('!');
+// } + 1; //Function Declaration
+
+//23
+// function func() {
+// 	console.log('!');
+// } + console.log('!'); //Function Declaration
