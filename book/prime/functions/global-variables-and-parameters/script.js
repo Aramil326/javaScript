cd2164870798c1960a3cd4d450374297dc20962f
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-19 13:16:20 +0300

    done

---
+++ book/prime/functions/global-variables-and-parameters/script.js
@@ -0,0 +1,60 @@
+// №1
+// function func(localNum) {
+//   console.log(localNum);
+// }
+
+// func(1); //1
+
+// №2
+// function func(localNum) {
+//   console.log(localNum);
+// }
+
+// let num = 1;
+// func(num); //1
+
+// №3
+// function func(localNum) {
+//   console.log(localNum);
+// }
+
+// let num = 1;
+// func(num); //1
+// num = 2;
+
+// №4
+// let num = 1;
+
+// function func(localNum) {
+//   console.log(localNum);
+// }
+
+// num = 2;
+// func(num); //2
+
+// №5
+// function func(localNum) {
+//   localNum = 2;
+// }
+
+// let num = 1;
+// func(num);
+// console.log(num); //1
+
+// №6
+// function func(localNum) {
+//   num = 2;
+// }
+
+// let num = 1;
+// func(num);
+// console.log(num); //2
+
+// №7
+// function func(localNum) {
+// 	let num = 2;
+// }
+
+// let num = 1;
+// func(num);
+// console.log(num); //1
