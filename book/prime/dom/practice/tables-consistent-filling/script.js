6ed369151352bb31af9949bf5885b120fa9b6058
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-20 12:34:48 +0300

    done

---
+++ book/prime/dom/practice/tables-consistent-filling/script.js
@@ -0,0 +1,35 @@
+//1
+// let table = document.querySelector('#table');
+
+// let k = 2; // начальное значение счетчика
+// for (let i = 0; i < 5; i++) {
+//   let tr = document.createElement('tr');
+
+//   for (let i = 0; i < 5; i++) {
+//     let td = document.createElement('td');
+//     td.textContent = k; // записываем счетчик в ячейку
+//     k++; // увеличиваем счетчик
+
+//     tr.appendChild(td);
+//   }
+
+//   table.appendChild(tr);
+// }
+
+//2
+// let table = document.querySelector('#table');
+
+// let k = 2; // начальное значение счетчика
+// for (let i = 0; i < 5; i++) {
+//   let tr = document.createElement('tr');
+
+//   for (let i = 0; i < 5; i++) {
+//     let td = document.createElement('td');
+//     td.textContent = k; // записываем счетчик в ячейку
+//     k += 2; // увеличиваем счетчик
+
+//     tr.appendChild(td);
+//   }
+
+//   table.appendChild(tr);
+// }
