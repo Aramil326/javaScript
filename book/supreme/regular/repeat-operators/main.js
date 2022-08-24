3a1563f784121fe6f88b0779d030aeeb5d0c1726
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-24 20:20:40 +0300

    done

---
+++ book/supreme/regular/repeat-operators/main.js
@@ -0,0 +1,15 @@
+// 1
+// let str = 'aa aba abba abbba abca abea';
+// console.log(str.replace(/ab+a/g, '!'))
+
+// 2
+// let str = 'aa aba abba abbba abca abea';
+// console.log(str.replace(/ab*a/g, '!'))
+
+// 3
+// let str = 'aa aba abba abbba abca abea';
+// console.log(str.replace(/ab?a/g, '!'))
+
+// 4
+// let str = 'aa aba abba abbba abca abea';
+// console.log(str.replace(/ab*a/g, '!'))
