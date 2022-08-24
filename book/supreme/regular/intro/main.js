e0d4f6cd44fd1cedc3ba2384b0a91fd55bea12b0
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-24 20:13:47 +0300

    done

---
+++ book/supreme/regular/intro/main.js
@@ -0,0 +1,9 @@
+// 1
+// let str = 'ahb acb aeb aeeb adcb axeb';
+// console.log(str.replace(/a.b/g, '!'))
+// 2
+// let str = 'aba aca aea abba adca abea';
+// console.log(str.replace(/a..a/g, '!'))
+// 3
+// let str = 'aba aca aea abba adca abea';
+// console.log(str.replace(/ab.a/g, '!'))
