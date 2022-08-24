810309d90721960bb76e44e6b9689938345daa67
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-24 20:36:11 +0300

    done

---
+++ book/supreme/regular/escaping-special-characters/main.js
@@ -0,0 +1,23 @@
+// 1
+// let str = 'a.a aba aea';
+// console.log(str.replace(/a\.a/g, '!'))
+
+// 2
+// let str = '2+3 223 2223';
+// console.log(str.replace(/2\+3/g, '!'))
+
+// 3
+// let str = '23 2+3 2++3 2+++3 345 567';
+// console.log(str.replace(/2\++3/g, '!'))
+
+// 4
+// let str = '23 2+3 2++3 2+++3 445 677';
+// console.log(str.replace(/2\+*3/g, '!'))
+
+// 5
+// let str = '*+ *q+ *qq+ *qqq+ *qqq qqq+';
+// console.log(str.replace(/\*q*.\+/g, '!'))
+
+// 6
+// let str = '[abc] {abc} abc (abc) [abc]';
+// console.log(str.replace(/\[...\]/g, '!'))
---
+++ book/supreme/regular/escaping-special-characters/readme.md
@@ -0,0 +1,1 @@
+https://code.mu/ru/javascript/book/supreme/regular/escaping-special-characters/
