efd463b9f3b0a802c9934a24d2a2e56aef2f6e82
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-24 20:39:41 +0300

    done

---
+++ book/supreme/regular/curly-braces/main.js
@@ -0,0 +1,11 @@
+// 1
+// let str = 'aa aba abba abbba abbbba abbbbba';
+// console.log(str.replace(/ab{2,4}a/g, '!'));
+
+// 2
+// let str = 'aa aba abba abbba abbbba abbbbba';
+// console.log(str.replace(/ab{0,3}a/g, '!'));
+
+// 3
+// let str = 'aa aba abba abbba abbbba abbbbba';
+// console.log(str.replace(/ab{4,}/g, '!'));
