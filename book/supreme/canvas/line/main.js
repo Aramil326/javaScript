5b5e30d2c6e950c963c56bacd027b3c91da0d909
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-26 12:40:44 +0300

    done

---
+++ book/supreme/canvas/line/main.js
@@ -0,0 +1,109 @@
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(100, 100);
+// ctx.lineTo(150, 50);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(100, 100);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(50, 50);
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(150, 150);
+// ctx.lineTo(50, 150);
+// ctx.closePath();
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(100, 100);
+// ctx.lineTo(150, 150);
+// //ctx.lineTo(50, 50); - этот шаг опускаем
+// ctx.closePath();
+// ctx.stroke();
+
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(100, 100);
+// ctx.lineTo(150, 50);
+// //ctx.lineTo(50, 50); - этот шаг опускаем
+// //ctx.closePath(); - и этот тоже
+// ctx.fill();
+
+// 1
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(100, 50);
+// ctx.stroke();
+
+// 2
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(50, 100);
+// ctx.stroke();
+
+// 3
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(100, 100);
+// ctx.moveTo(100, 50);
+// ctx.lineTo(50, 100);
+// ctx.stroke();
+
+// 4
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(50, 100);
+// ctx.lineTo(100, 100);
+// ctx.lineTo(100, 50);
+// ctx.lineTo(50, 50);
+// ctx.stroke();
+
+// 5
+// ctx.beginPath();
+// ctx.moveTo(100, 50);
+// ctx.lineTo(150, 100);
+// ctx.lineTo(50, 100);
+// ctx.lineTo(100, 50);
+// ctx.stroke();
+
+// 6
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(125, 25);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(125, 75);
+// ctx.stroke();
+
+// 7
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(50, 100);
+// ctx.lineTo(100, 100);
+// ctx.lineTo(100, 50);
+// ctx.lineTo(50, 50);
+// ctx.fill()
+// ctx.stroke();
+
+// 8
+ctx.beginPath();
+ctx.moveTo(100, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(100, 50);
+ctx.fill()
+ctx.stroke();
