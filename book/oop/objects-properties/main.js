3b748c4cf55b1479b2fad65c744afc0f25b60b95
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-27 14:44:57 +0300

    done

---
+++ book/oop/objects-properties/main.js
@@ -0,0 +1,15 @@
+class Employee {
+  name = 'John';
+  age = 35;
+  salary = 350;
+}
+
+let employee = new Employee
+
+employee.name = 'John'
+employee.age = 35
+employee.salary = 350
+
+console.log(employee.name)
+console.log(employee.age)
+console.log(employee.salary)
