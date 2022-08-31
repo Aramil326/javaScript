1dd7bd5a55d3247422d549794fc4ff86887280c1
Shaimardanov Aramil <aramil28022005@gmail.com>
2022-08-31 15:47:21 +0300

    done

---
+++ book/oop/objects-comparison/main.js
@@ -0,0 +1,84 @@
+// 1
+// class Employee {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 === emp2); //false
+
+// 2
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 === emp1); //true
+
+// 3
+// class Employee {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('john');
+
+// console.log(emp1 === emp2); //false
+
+// 4
+// class Employee {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 !== emp1); //false
+
+// 5
+// class Employee {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = emp1;
+
+// console.log(emp1 === emp2); //true
+
+// 6
+// class Employee {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 !== emp2); //true
+
+// 7
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = emp1;
+// emp2.name = 'eric';
+
+// console.log(emp1 === emp2); //true
